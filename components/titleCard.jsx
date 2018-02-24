12761b764b477a625e37b047ae768f5a88532ab6
Scruggs <kid.idioteque@gmail.com>
2018-02-23 17:54:53 -0800

    improves titleCard code

--- components/titleCard.jsx
+++ components/titleCard.jsx
@@ -1,41 +1,54 @@
 import React from 'react'
-import $ from "jquery";
 
 class TitleCard extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {visible: true}
+        this.toggle = this.toggle.bind(this)
+    }
+
+    toggle() {
+        this.setState({visible: !this.state.visible})
+    }
+
     componentDidMount() {
-
-        const turnOff = () => {
-            $("#titleCard").hide()
-        }
-
-        let section = window.audioAnalysis.sections.find( (section) => {
+        let {sections} = window.audioAnalysis
+        let section = sections.find( (section) => {
           return section.start > 12
         })
-
         let duration = section.start * 1000
-
         let progressMs = window.currentTrack.progress_ms
+
+        //intro card
         window.networkDelay = Date.now() - window.beginT
         duration = duration - progressMs - window.networkDelay
-        window.setTimeout(turnOff, duration)
-        
-        for(let i=0;i<3;i++){
-            $('.glitch .text span').eq(0).clone().prependTo('.glitch .text');
-        }
+        window.setTimeout(this.toggle, duration)
+
+        //outro 
+        section = sections[sections.length - 1]
+        let timeStamp = section.start * 1000 - progressMs - window.networkDelay
+        window.setTimeout(this.toggle, timeStamp)
+        console.log("outro: ", timeStamp, section)
     }
 
     render() {
         let {artist, songTitle} = this.props
-        return <section id="titleCard">
+        let text = []
+        for(let i = 0; i < 4; i++) { text.push(<span key={i}>
+            "{songTitle}"
+            <br/>
+            {artist}
+        </span>)
+        }
+        let visible = this.state.visible
+        let style = {display: visible ? null : "none"}
+
+        return <section id="titleCard" style={style}>
             <div className="glitch">
                 <div className="text">
-                    <span>
-                    "{songTitle}"
-                    <br/>
-                    {artist}
-                    </span>
+                    {text}
                 </div>
-                            </div>
+            </div>
         </section>
     }
   }
